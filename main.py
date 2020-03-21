6a7dc64d8365acfb8ea4368d7ca3cf2473df4669
maryeleanor <marycasserly@gmail.com>
2020-03-20 18:18:05 -0700

    cleaning up app config files

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-from app import app
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Phil'}
+    return render_template("index.html", title='Quarantine Ideas', user=user)
+
