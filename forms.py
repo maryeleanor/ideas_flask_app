0a62dcdb90b730c75381f5e71d0fc441ff43a689
maryeleanor <marycasserly@gmail.com>
2020-03-21 17:21:49 -0700

    added forms and valildation

---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length (min=3, max=30)])
+    confirm_password = PasswordField('Confrim Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=3, max=30)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,15 +1,74 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '348943509rf4j09w09809cv987v987b'
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
+ideas = [
+    {
+        'suggestion': 'knitting',
+        'for': 'creativity, managing anxiety, learning something new, calming', 
+        'notes': 'if you have yarn around, its very easy to learn.',
+        'by': 'Afi'
+    },
+    {
+        'suggestion': 'go for a walk',
+        'for': 'managing anxiety, slowing down',
+        'notes': 'feeling the breeze',
+        'by': 'Robyn'
+    },
+    {
+        'suggestion': 'jigsaw puzzles',
+        'for': 'passing time',
+        'notes': "There are some free sites online if you don't have puzzles on hand",
+        'by': 'Brisa'
+    }
+]
+
+
 @app.route('/')
 @app.route('/index')
-def index():
+@app.route('/home')
+def home():
     user = {'username': 'Phil'}
-    return render_template("index.html", title='Quarantine Ideas', user=user)
+    return render_template("index.html", title='Quarantine Ideas', user=user, ideas=ideas)
+
+
+@app.route('/about')
+def about():    
+    return render_template("about.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) 
+
+    return render_template("register.html",  title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@test.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home')) 
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html", title='Login', form=form)
+
+
+
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
 
