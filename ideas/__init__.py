165557a8a55c90863b929ce3c132355d18779e29
maryeleanor <marycasserly@gmail.com>
2020-03-28 21:39:32 -0700

    optimized for mobile, updated copy

--- ideas/__init__.py
+++ ideas/__init__.py
@@ -1,5 +1,4 @@
-
-import os
+ 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
@@ -9,14 +8,9 @@ app = Flask(__name__)
 # secret key for users to stay logged in
 app.config['SECRET_KEY'] = '348943509rf4j09w09809cv987v987b'
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
 # config database with sqlalchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ideas.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) 
-  
- 
-
+   
 from ideas import routes
--- ideas/routes.py
+++ ideas/routes.py
@@ -1,7 +1,5 @@
-import os
-import random
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
+
+import random  
 from flask import render_template, url_for,  redirect, request
 from ideas import app, db
 
@@ -16,81 +14,66 @@ read = db.Table('read', db.metadata, autoload=True, autoload_with=db.engine)
 grateful = db.Table('grateful', db.metadata, autoload=True, autoload_with=db.engine)
 health = db.Table('health', db.metadata, autoload=True, autoload_with=db.engine)
 
-# form for buttons
-class ideaForm(FlaskForm):
-    cookField = SubmitField('Something to cook')
-    doField = SubmitField('Something to do')
-    donateField = SubmitField('Somewhere to donate')
-    kidsField = SubmitField('Something to do with kids')
-    listenField = SubmitField('Something to listen to')
-    watchField = SubmitField('Something to watch')
-    readField = SubmitField('Something to read')
-    gratefulField = SubmitField('Something to be grateful for')
-    healthField = SubmitField('Something for your mental health')
-
-
 @app.route('/')
 @app.route('/index')
 @app.route('/home', methods=['GET', 'POST'])
-def home():
-    form = ideaForm()
-
+def home(): 
     if request.method == "POST":
-        if form.cookField.data:
+        if request.form.get('cook') == 'cook': 
             cook_ideas = db.session.query(cook).all()
             x = random.randint(0, len(cook_ideas)-1)
             idea = cook_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
 
-        if form.doField.data:
+        if request.form.get('do') == 'do':
             do_ideas = db.session.query(do).all()
             x = random.randint(0, len(do_ideas)-1)
             idea = do_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
 
-        if form.kidsField.data:
+        if request.form.get('kids') == 'kids':
             kids_ideas = db.session.query(kids).all()
             x = random.randint(0, len(kids_ideas)-1)
             idea = kids_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
 
-        if form.watchField.data:
+        if request.form.get('watch') == 'watch':  
             watch_ideas = db.session.query(watch).all()
             x = random.randint(0, len(watch_ideas)-1)
             idea = watch_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
 
-        if form.readField.data:
+        if request.form.get('read') == 'read':  
             read_ideas = db.session.query(read).all()
             x = random.randint(0, len(read_ideas)-1)
             idea = read_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
 
-        if form.listenField.data:
+        if request.form.get('listen') == 'listen':  
             listen_ideas = db.session.query(listen).all()
             x = random.randint(0, len(listen_ideas)-1)
             idea = listen_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
-    
-        if form.gratefulField.data:
+            return render_template("index.html", idea=idea)
+
+        if request.form.get('grateful') == 'grateful': 
             grateful_ideas = db.session.query(grateful).all()
             x = random.randint(0, len(grateful_ideas)-1)
             idea = grateful_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
 
-        if form.donateField.data:
+        if request.form.get('donate') == 'donate': 
             donate_ideas = db.session.query(donate).all()
             x = random.randint(0, len(donate_ideas)-1)
             idea = donate_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
 
-        if form.healthField.data:
+        if request.form.get('health') == 'health':  
             health_ideas = db.session.query(health).all()
             x = random.randint(0, len(health_ideas)-1)
             idea = health_ideas[x]
-            return render_template("index.html", title='Pandemic Tool Kit', idea=idea, form=form)
+            return render_template("index.html", idea=idea)
     
-    return render_template("index.html", title='Pandemic Tool Kit', form=form)
+    return render_template("index.html")
 
 
  
