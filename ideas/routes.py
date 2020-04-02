6eb25dac5f193a243e2e1f23ae5532054569db42
maryeleanor <marycasserly@gmail.com>
2020-04-01 17:33:03 -0700

    cleaned up routes file

--- ideas/routes.py
+++ ideas/routes.py
@@ -14,64 +14,58 @@ read = db.Table('read', db.metadata, autoload=True, autoload_with=db.engine)
 grateful = db.Table('grateful', db.metadata, autoload=True, autoload_with=db.engine)
 health = db.Table('health', db.metadata, autoload=True, autoload_with=db.engine)
 
+
+def getIdea(category):
+    ideas = db.session.query(category).all()
+    x = random.randint(0,len(ideas)-1)
+    idea = ideas[x]
+    return idea
+
+
 @app.route('/')
 @app.route('/index')
 @app.route('/home', methods=['GET', 'POST'])
 def home(): 
     if request.method == "POST":
-        if request.form.get('cook') == 'cook': 
-            cook_ideas = db.session.query(cook).all()
-            x = random.randint(0, len(cook_ideas)-1)
-            idea = cook_ideas[x]
-            return render_template("index.html", idea=idea, category="cook", tag="Something to cook")
+        category = request.form.get('category')
 
-        if request.form.get('do') == 'do':
-            do_ideas = db.session.query(do).all()
-            x = random.randint(0, len(do_ideas)-1)
-            idea = do_ideas[x]
-            return render_template("index.html", idea=idea, category="do", tag="Something to do")
+        if category == 'cook':  
+            idea = getIdea(cook)
+            tag = "Something to Cook"
+             
+        if category == 'do':
+            idea = getIdea(do)
+            tag = "Something to Do" 
 
-        if request.form.get('kids') == 'kids':
-            kids_ideas = db.session.query(kids).all()
-            x = random.randint(0, len(kids_ideas)-1)
-            idea = kids_ideas[x]
-            return render_template("index.html", idea=idea, category="kids", tag="Something to do with kids")
+        if category == 'kids':
+            idea = getIdea(kids)
+            tag = "Something to do with kids" 
+        
+        if category == 'watch':  
+            idea = getIdea(kids)
+            tag = "Something to watch"
 
-        if request.form.get('watch') == 'watch':  
-            watch_ideas = db.session.query(watch).all()
-            x = random.randint(0, len(watch_ideas)-1)
-            idea = watch_ideas[x]
-            return render_template("index.html", idea=idea, category="watch", tag="Something to watch")
+        if category == 'read':  
+            idea = getIdea(read)
+            tag = "Something to read" 
 
-        if request.form.get('read') == 'read':  
-            read_ideas = db.session.query(read).all()
-            x = random.randint(0, len(read_ideas)-1)
-            idea = read_ideas[x]
-            return render_template("index.html", idea=idea, category="read", tag="Something to read")
+        if category == 'listen':
+            idea = getIdea(listen)
+            tag = "Something to listen to"
+            
+        if category == 'grateful':
+            idea = getIdea(grateful)
+            tag="Something to be grateful for"
+        
+        if category == 'health':
+            idea = getIdea(health)
+            tag="For your mental health"
 
-        if request.form.get('listen') == 'listen':  
-            listen_ideas = db.session.query(listen).all()
-            x = random.randint(0, len(listen_ideas)-1)
-            idea = listen_ideas[x]
-            return render_template("index.html", idea=idea, category="listen", tag="Something to listen to")
+        if category == 'donate':
+            idea = getIdea(donate)
+            tag="Something to donate"
 
-        if request.form.get('grateful') == 'grateful': 
-            grateful_ideas = db.session.query(grateful).all()
-            x = random.randint(0, len(grateful_ideas)-1)
-            idea = grateful_ideas[x]
-            return render_template("index.html", idea=idea, category="grateful", tag="Something to be grateful for")
-
-        if request.form.get('health') == 'health':
-            health_ideas = db.session.query(health).all()
-            x = random.randint(0, len(health_ideas)-1)
-            idea = health_ideas[x]
-            return render_template("index.html", idea=idea, category="health", tag="For your mental health")
-
-        if request.form.get('donate') == 'donate': 
-            donate_ideas = db.session.query(donate).all()
-            x = random.randint(0, len(donate_ideas)-1)
-            idea = donate_ideas[x]
-            return render_template("index.html", idea=idea, category="donate", tag="Something to donate")
+        return render_template("index.html", idea=idea, category=category, tag=tag)
 
     return render_template("index.html")
 
